7d42e0ba287366ee33c41f96907309f430b58a7e
ubaid0309 <ubaidskms@gmail.com>
2023-12-29 17:36:31 +0530

    setup : tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
